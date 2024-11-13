42b4269ab734f207d06badc5cf7f4f74f7a3caed
Pinchas <139820013+pmk248@users.noreply.github.com>
2024-11-13 13:51:03 +0200

    Register endpoint competed (bugged)

---
+++ src/controllers/authControllers/register.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import AppResError from "../../types/extensions/app.res.error";
+import { RegisterDTO } from "../../types/DTOs/auth";
+import { User } from "../../types/schemas/userSchema";
+import { Organization } from "../../types/schemas/organizationSchema";
+
+const register = async (req: Request<any, any, RegisterDTO>, res: Response) => {
+    try {
+        // Finding Organization:
+        const organization = await Organization.findOne({ name: req.body.organization });
+        if (!organization) {
+            throw new AppResError(404, `${req.body.organization} doesn't exist!`);
+        }
+
+        const role = organization.name.startsWith("IDF") ? "IDF" : "Attacker";
+
+        // Creating new User:
+        const user = new User({
+            username: req.body.username,
+            password: await bcrypt.hash(req.body.password, 5),
+            role,
+            organization: organization._id,
+            region: role === "IDF" ? req.body.region : undefined
+        });
+
+        // Saving new User:
+        await user.save();
+        res.status(201).send(`${user.username} registered successfully`);
+    } catch (err) {
+        const error = err as AppResError;
+        res.status(error.statusCode || 500).send(error.message || "Server error");
+        console.error(error);
+    }
+};
+
+export default register;
